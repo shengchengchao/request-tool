32e3a954aab515e6e67a977e2df2f752cbe21600
shengchengchao <shengchengchao@ewell.cc>
2021-03-12 14:13:40 +0800

    feat(start): 提交基础类
    提交基础类型

---
+++ src/main/java/com/xixi/request/tool/annonation/UserIdVerification.java
@@ -0,0 +1,9 @@
+package com.xixi.request.tool.annonation;
+
+/**
+ * @author shengchengchao
+ * @Description userId 的校验 有问题将直接返回
+ * @createTime 2021/3/12
+ */
+public @interface UserIdVerification {
+}
---
+++ src/main/java/com/xixi/request/tool/common/ResultBean.java
@@ -0,0 +1,9 @@
+package com.xixi.request.tool.common;
+
+/**
+ * @author shengchengchao
+ * @Description 公用的返回类
+ * @createTime 2021/3/12
+ */
+public class ResultBean {
+}
