3b382079cc2012b6ec11ddf486d4248c55c9e168
shengchengchao <shengchengchao@ewell.cc>
2021-03-24 15:12:08 +0800

    feat(start): 添加注解

--- src/main/java/com/xixi/request/tool/RequsetToolApplication.java
+++ src/main/java/com/xixi/request/tool/RequsetToolApplication.java
@@ -3,9 +3,11 @@ package com.xixi.request.tool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @ServletComponentScan
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class RequsetToolApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/xixi/request/tool/aspect/TimeAspect.java
+++ src/main/java/com/xixi/request/tool/aspect/TimeAspect.java
@@ -7,6 +7,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -18,6 +19,7 @@ import java.util.Date;
  */
 @Aspect
 @Slf4j
+@Component
 public class TimeAspect {
 
     @Around("bean(*Controller)")
